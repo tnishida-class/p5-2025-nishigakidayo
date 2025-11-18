dd167a0e1dac25ff811d4b679e2ec4b7a7ad5a25
nishigakidayo <2440824h@gsuite.kobe-u.ac.jp>
2025-11-18 10:57:52 +0900

    section3-1 changed

--- Q3-1/sketch.js
+++ Q3-1/sketch.js
@@ -1,6 +1,7 @@
 // 心臓の鼓動のようなアニメーション
 const cycle = 100; // 1周期のフレーム数
-let count; // 何フレーム目か
+let count = 0; // 何フレーム目か
+let size = 50;
 
 function setup(){
   createCanvas(200, 200);
@@ -10,11 +11,20 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   
-  let speed = 1; // アニメーションの速さ
+  let speed // アニメーションの速さ
   // BLANK[2]
+  if(keyIsPressed){
+    speed = 2
+  }
+  else{
+    speed = 1
+  }
   count = (count + speed) % cycle;
-
-  let size = 50;
+  if(count < cycle / 2){
+    size = 50 + cycle - count;
+  }else{
+    size = 50 + count;
+  }
   // BLANK[1] 1周期の前半は size が大きくなり、後半は小さくなる
-  ellipse(width / 2, height / 2, size);
+  ellipse(width / 2, height / 2, size, size);
 }
