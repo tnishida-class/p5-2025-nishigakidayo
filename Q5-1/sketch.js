303051cc85d810ab937a6a133b6165f53b2bfdb9
nishigakidayo <2440824h@gsuite.kobe-u.ac.jp>
2026-01-06 12:19:48 +0900

    5-1 changed

--- Q5-1/sketch.js
+++ Q5-1/sketch.js
@@ -1,14 +1,25 @@
 // EUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(2, 0, 153);
   noStroke();
   for(let i = 0; i < 12; i++){
     const theta = TWO_PI * i / 12;
     const x = 100 + cos(theta) * 50;
     const y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    fill(255, 200, 0);
+    star(x, y, 10);
   }
 }
 
 // BLANK[1] ヒント：star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
+function star(cx, cy, r){
+  beginShape();    // 点つなぎを始める
+  for(let i = 0; i < 5; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    const x = cx + cos(theta) * r;
+    const y = cy + sin(theta) * r;
+    vertex(x, y);  // 次につなぐ点を１つ増やす
+  }
+  endShape(CLOSE); // 点つなぎを終わる
+}
